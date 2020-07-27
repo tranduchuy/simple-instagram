eaabf961174475973a0941fd794e053c180dac8e
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-07-27 19:52:34 +0700

    create api file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import connectDB from './db';
+import {api} from "./routes/api";
 
 const init = (): void => {
     const app = express();
     app.use(express.json());
+    app.use('/api', api);
 
     app.listen(process.env.app_port, function () {
         console.log(`App is listening on port ${process.env.app_port}`);
