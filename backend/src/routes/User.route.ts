f468f653c90984fc100ae1fac30268ddeb45b8c7
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-08-01 14:30:07 +0700

    add path api login

--- backend/src/routes/User.route.ts
+++ backend/src/routes/User.route.ts
@@ -4,5 +4,6 @@ import {userCtrl} from "../constrollers/User.controller";
 const router = express.Router({});
 
 router.post('/register', userCtrl.registerNewUser);
+router.post('/login', userCtrl.userLogin);
 
 export const UserRoute = router;
