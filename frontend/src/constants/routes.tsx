4f05f9c3c6454338b082861ca2cf4da0b0c46626
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-08-02 11:37:57 +0700

    add path of Login route

--- frontend/src/constants/routes.tsx
+++ frontend/src/constants/routes.tsx
@@ -5,16 +5,11 @@ export const routes = [
         path: '/register',
         name: 'Register',
         component: Register
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
     }
-    // {
-    //     path: '/login',
-    //     name: 'Login',
-    //     component: ''
-    // },
-    // {
-    //     path: '/home',
-    //     name: 'Home',
-    //     component: ''
-    // }
 ];
 
