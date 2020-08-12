7c81aabd41aab269dc11faae583eed2cf1983963
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-08-12 10:25:28 +0700

    update handle confirm user

--- frontend/src/pages/Confirm/confirm.tsx
+++ frontend/src/pages/Confirm/confirm.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {RouteComponentProps} from "react-router-dom";
-import {Container} from "react-bootstrap";
+import {Container, Alert, Col, Row, Form} from "react-bootstrap";
 import axios, {AxiosResponse} from "axios";
 import *as API from "../../constants/api";
 import queryString from 'query-string';
 
 interface ConfirmProps extends RouteComponentProps {
 
+}
+
+type ResponseMessage = {
+    successMessage?: string;
+    errorMessage?: string;
 }
 
 type ConfirmFormData = {
@@ -17,10 +22,14 @@ type ConfirmResSuccess = {
     message: string;
 };
 
-export class Confirm extends React.Component<ConfirmProps, any> {
+export class Confirm extends React.Component<ConfirmProps, ResponseMessage> {
     constructor(props: ConfirmProps) {
         super(props);
-    };
+        this.state = {
+            successMessage: "",
+            errorMessage: ""
+        };
+    }
 
     componentDidMount(): void {
         this.handleVerifyEmail();
@@ -39,13 +48,16 @@ export class Confirm extends React.Component<ConfirmProps, any> {
 
         axios.post<ConfirmResSuccess, AxiosResponse<ConfirmResSuccess>>(API.Confirm, formData)
             .then((res) => {
-                alert(res.data.message);
-                this.props.history.push('/');
+                this.setState({
+                    successMessage: res.data.message
+                });
+                setTimeout(() => this.props.history.push('/'), 2000)
             })
             .catch((err) => {
                 if(err.response.data.message) {
-                    alert(err.response.data.message);
-
+                    this.setState({
+                        errorMessage: err.response.data.message
+                    });
                 } else {
                     alert(err);
                 }
@@ -56,7 +68,21 @@ export class Confirm extends React.Component<ConfirmProps, any> {
     render(): React.ReactElement {
         return (
             <Container fluid={true}>
-
+                <Form>
+                    <Form.Row className="align-items-center">
+                        <Col sm={{ offset: 4, span: "auto" }} >
+                            {this.state.successMessage ? (
+                                <Alert variant="success" className="mt-5">
+                                    <p>{this.state.successMessage}</p>
+                                </Alert>
+                            ): (
+                                <Alert variant="danger" className="mt-5">
+                                    <p>{this.state.errorMessage}</p>
+                                </Alert>
+                            )}
+                        </Col>
+                    </Form.Row>
+                </Form>
             </Container>
         );
     }
