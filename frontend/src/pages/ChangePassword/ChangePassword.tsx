e8a40c0f98dd4e567b32f8e06545cad7dc9ef7f3
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-09-12 11:58:15 +0700

    handle feature change password

--- frontend/src/pages/ChangePassword/ChangePassword.tsx
+++ frontend/src/pages/ChangePassword/ChangePassword.tsx
@@ -1,8 +1,11 @@
+import axios, { AxiosResponse } from 'axios';
+import queryString from 'query-string';
 import React from 'react';
 import {
-    Col, Container, Form, Button,
+    Col, Container, Form, Button, Alert,
 } from 'react-bootstrap';
 import { Link, RouteComponentProps } from 'react-router-dom';
+import * as API from '../../constants/api';
 import styles from './ChangePassword.module.scss';
 
 type ChangePasswordProps = RouteComponentProps;
@@ -11,9 +14,11 @@ type ChangePasswordState = {
     password: string;
     confirmPassword: string;
     errorMessage: string;
+    errorTokenMessage: string;
 };
 
 type ChangePasswordFormData = {
+    forgotPasswordToken: string;
     password: string;
     confirmPassword: string;
 };
@@ -23,7 +28,133 @@ type ChangePasswordResSuccess = {
 };
 
 export class ChangePassword extends React.Component<ChangePasswordProps, ChangePasswordState> {
+    state: ChangePasswordState = {
+        password: '',
+        confirmPassword: '',
+        errorMessage: '',
+        errorTokenMessage: '',
+    };
+
+    componentDidMount(): void {
+        this.handleCheckToken();
+    }
+
+    handleCheckToken = (): void => {
+        const value = queryString.parse(this.props.location.search);
+        const query: string = value.forgotPasswordToken as string;
+
+        const formDataCheckToken: object = {
+            forgotPasswordToken: query,
+        };
+
+        axios.post<ChangePasswordResSuccess, AxiosResponse<ChangePasswordResSuccess>>(API.ChangePass, formDataCheckToken)
+            .then()
+            .catch((err) => {
+                if (err.response.data.message) {
+                    this.setState({
+                        errorTokenMessage: err.response.data.message,
+                    });
+                }
+            });
+    };
+
+    onSubmit = (event: React.FormEvent<HTMLInputElement>): void => {
+        event.preventDefault();
+        const { password, confirmPassword } = this.state;
+        const value = queryString.parse(this.props.location.search);
+        const query: string = value.forgotPasswordToken as string;
+
+        const formData: ChangePasswordFormData = {
+            forgotPasswordToken: query,
+            password,
+            confirmPassword,
+        };
+
+        if (password.length < 6) {
+            this.setState({
+                errorMessage: 'Create a password at least 6 characters long.',
+            });
+            return;
+        }
+
+        axios.post<ChangePasswordResSuccess, AxiosResponse<ChangePasswordResSuccess>>(API.ChangePass, formData)
+            .then(() => {
+                setTimeout(() => this.props.history.push('/'), 1000);
+            })
+            .catch((err) => {
+                if (err.response.data.message) {
+                    this.setState({
+                        errorMessage: err.response.data.message,
+                    });
+                }
+            });
+    }
+
+    handlePasswordInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        this.setState({
+            password: event.target.value,
+        });
+    };
+
+    handleConfirmPasswordInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        this.setState({
+            confirmPassword: event.target.value,
+        });
+    };
+
     render(): JSX.Element {
+        const {
+            state,
+            onSubmit,
+            handlePasswordInputChange,
+            handleConfirmPasswordInputChange,
+        } = this;
+        const showContentInput = (
+            <Col sm={{ offset: 1, span: '7' }}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>New password:</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={state.password}
+                        onChange={handlePasswordInputChange}
+                    />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>New password confirmation:</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={state.confirmPassword}
+                        onChange={handleConfirmPasswordInputChange}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Change Password
+                </Button>
+            </Col>
+        );
+        let showContent;
+        if (state.errorTokenMessage) {
+            showContent = (
+                <Col sm={{ offset: 1, span: '7' }}>
+                    <Alert variant="danger">
+                        <p className="mb-0">{state.errorTokenMessage}</p>
+                    </Alert>
+                </Col>
+            );
+        } else if (state.errorMessage) {
+            showContent = (
+                <>
+                    <Col sm={{ offset: 1, span: '7' }}>
+                        <Alert variant="danger">
+                            <p className="mb-0">{state.errorMessage}</p>
+                        </Alert>
+                    </Col>
+                    {showContentInput}
+                </>
+            );
+        } else {
+            showContent = showContentInput;
+        }
         return (
             <div className={styles.pageBody}>
                 <div className={styles.content}>
@@ -37,25 +168,9 @@ export class ChangePassword extends React.Component<ChangePasswordProps, ChangeP
                             <div className={styles.sidebar} />
                             <div className={styles.mainContent}>
                                 <Container fluid>
-                                    <Form>
+                                    <Form onSubmit={onSubmit}>
                                         <Form.Row className="align-items-center">
-                                            <Col sm={{ offset: 1, span: '7' }}>
-                                                <Form.Group controlId="formBasicEmail">
-                                                    <Form.Label>New password:</Form.Label>
-                                                    <Form.Control
-                                                        type="password"
-                                                    />
-                                                </Form.Group>
-                                                <Form.Group controlId="formBasicPassword">
-                                                    <Form.Label>New password confirmation:</Form.Label>
-                                                    <Form.Control
-                                                        type="password"
-                                                    />
-                                                </Form.Group>
-                                                <Button variant="primary" type="submit">
-                                                    Change Password
-                                                </Button>
-                                            </Col>
+                                            {showContent}
                                         </Form.Row>
                                     </Form>
                                 </Container>
