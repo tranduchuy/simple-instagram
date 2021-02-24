a174ee729b431d952a046e989dbe96e2ff030244
nguyenvu <nguyenvu.hcmus@gmail.com>
2021-02-24 23:27:09 +0700

    create bookmark model

---
+++ packages/backend/src/models/bookmark.model.ts
@@ -0,0 +1,14 @@
+import {
+    createSchema, ExtractDoc, ExtractProps, Type, typedModel,
+} from 'ts-mongoose';
+import { PostSchema } from './post.model';
+import { UserSchema } from './user.model';
+
+export const BookmarkSchema = createSchema({
+    postId: Type.ref(Type.objectId()).to('Post', PostSchema),
+    userId: Type.ref(Type.objectId()).to('User', UserSchema),
+}, { timestamps: true });
+
+export const BookmarkModel = typedModel('Bookmark', BookmarkSchema, 'bookmarks');
+export type BookmarkDoc = ExtractDoc<typeof BookmarkSchema>;
+export type Bookmark = ExtractProps<typeof BookmarkSchema>;
